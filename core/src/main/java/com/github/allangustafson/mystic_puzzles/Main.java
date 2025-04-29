dcbb880797342e6a03527b04f4388ee526934d22
Allan Gustafson <allan.s.gustafson@gmail.com>
2025-04-28 22:21:42 -0400

    fix some deprecated gradle features.

--- core/src/main/java/com/github/allangustafson/mystic_puzzles/Main.java
+++ core/src/main/java/com/github/allangustafson/mystic_puzzles/Main.java
@@ -1,29 +1,46 @@
 package com.github.allangustafson.mystic_puzzles;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends ApplicationAdapter {
+    private Skin skin;
+    private Stage stage;
     private SpriteBatch batch;
     private Texture image;
 
     @Override
     public void create() {
+        skin = new Skin(Gdx.files.internal("ui/uiskin.json"));
+        stage = new Stage(new ScreenViewport());
+        Gdx.input.setInputProcessor(stage);
+
         batch = new SpriteBatch();
         image = new Texture("libgdx.png");
+
+        TextButton textButton = new TextButton("Hello", skin);
+        textButton.setPosition(stage.getWidth()/2, stage.getHeight()/2, Align.center);
+        stage.addActor(textButton);
     }
 
     @Override
     public void render() {
-        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
-        batch.begin();
-        batch.draw(image, 140, 210);
-        batch.end();
+        Gdx.gl.glClearColor(.9f, .9f, .9f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        stage.act();
+        stage.draw();
+        //super.render();
     }
 
     @Override
